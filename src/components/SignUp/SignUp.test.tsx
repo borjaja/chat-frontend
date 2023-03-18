e6671bd2f22c5e53ba4e36dc1af3f5602f98463c
Borja <borjaalvarez_18@hotmail.com>
2023-03-18 22:43:30 +0100

    update tests

--- src/components/SignUp/SignUp.test.tsx
+++ src/components/SignUp/SignUp.test.tsx
@@ -11,23 +11,26 @@ import * as fn from "../../services/auth/auth";
 describe("SignUp Component", () => {
   it("Submitting with an empty username shows an error on the screen", async () => {
     render(<SignUp />);
+
     userEvent.click(screen.getByAltText("Write password"));
     userEvent.keyboard("pass");
     userEvent.click(screen.getByText("Sign up"));
+
     expect(screen.getByText("Error")).toBeInTheDocument();
   });
 
   it("Submitting with an empty password shows an error on the screen", async () => {
     render(<SignUp />);
+
     userEvent.click(screen.getByAltText("Write username"));
     userEvent.keyboard("user");
     userEvent.click(screen.getByText("Sign up"));
+
     expect(screen.getByText("Error")).toBeInTheDocument();
   });
 
   it("Submitting the form with the data of an already registered user shows an error on the screen", async () => {
     const spy = jest.spyOn(fn, "signUp").mockImplementation(() => Promise.resolve({ response: "Error" }));
-
     render(<SignUp />);
 
     userEvent.click(screen.getByAltText("Write username"));
